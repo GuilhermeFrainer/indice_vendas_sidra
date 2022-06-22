2e45bd4ac1f7a490653ac0a1dd8ffe29b32bf521
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2022-06-22 18:58:59 -0300

    Still not working

--- indice_vendas.py
+++ indice_vendas.py
@@ -2,7 +2,16 @@ from sidrapy import get_table
 
 
 def main():
-    varejo = get_table(table_code="8185", territorial_level="1", ibge_territorial_code="all", verify_ssl=False)
+    varejo = get_table(
+        table_code="8185", 
+        territorial_level="1",
+        ibge_territorial_code="1",
+        variable="11707",
+        header="n",
+        format="list",
+        period="202204"
+    )
+    print(varejo)
 
 
 if __name__ == '__main__':
