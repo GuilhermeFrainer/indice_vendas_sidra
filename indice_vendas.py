7dcdf68a8769ea6457b8cf9a53dae144c198623f
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2022-06-22 21:40:56 -0300

    Added get_period and get_data skeleton

--- indice_vendas.py
+++ indice_vendas.py
@@ -1,20 +1,56 @@
 from sidrapy import get_table
+from user_config import *
+import datetime
+import json
 
 
 def main():
-    varejo = get_table(
+
+    period = get_period()
+
+    #print(json.dumps(varejo, indent=4))
+
+
+# Returns period string going from chosen start date to current month
+def get_period():
+    
+    today = datetime.date.today()
+    start_date = datetime.date.fromisoformat(START_DATE)
+    return f"{start_date.year}{start_date.month}-{today.year}{today.month}"
+
+
+def get_data(period : str):
+    
+    retail = get_table(
         table_code="8185", 
         territorial_level="1",
         ibge_territorial_code="1",
         variable="11707",
         header="n",
         format="list",
-        period="202204",
-        classifications= {"11046": "56734"},
+        period=period,
+        classifications={"11046": "56734"},
     )
-    print(varejo)
 
+    ext_retail = get_table(
+        table_code="8186",
+        territorial_level="1",
+        ibge_territorial_code="1"
+    )
 
+    industry = get_table(
+        table_code="8159",
+        territorial_level="1",
+        ibge_territorial_code="1"
+    )
+
+    services = get_table(
+        table_code="8161",
+        territorial_level="1",
+        ibge_territorial_code="1"
+    )
+
+    
 if __name__ == '__main__':
     main()
 
