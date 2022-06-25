f26daa07b3d30308bc039d947d7c9b9cde4d45d5
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2022-06-25 15:18:37 -0300

    Added working list_to_workbook

--- indice_vendas.py
+++ indice_vendas.py
@@ -1,7 +1,9 @@
+from heapq import merge
 from sidrapy import get_table
 from user_config import *
 from index import Index
 import datetime
+import xlsxwriter
 import json
 
 
@@ -10,6 +12,39 @@ def main():
     period = get_period()
     indices = get_data(period)
 
+    workbook, worksheet = list_to_workbook(indices)
+
+    workbook.close()
+
+
+# Takes the data and puts it into an Excel file. Returns workbook and worksheet
+def list_to_workbook(indices : list[Index]):
+    
+    workbook = xlsxwriter.Workbook('Índices de vendas.xlsx')
+    worksheet = workbook.add_worksheet('Dados')
+
+    # Writes headers for unadjusted data
+    merge_format = workbook.add_format({'align': 'center'})
+    
+    worksheet.write('A5', 'Mês')
+    worksheet.write('B5', 'Varejo')
+    worksheet.write('C5', 'Varejo ampliado')
+    worksheet.write('D5', 'Indústria')
+    worksheet.write('E5', 'Serviços')
+
+    worksheet.merge_range('B4:E4', 'Inalterados', merge_format)
+
+    # Writes the data into the worksheet
+    date_format = workbook.add_format({'num_format': 'mmm-yy'})
+    
+    for i in range(len(indices)):
+        worksheet.write_datetime(f'A{6 + i}', indices[i].date, date_format)
+        worksheet.write(f'B{6 + i}', indices[i].retail)
+        worksheet.write(f'C{6 + i}', indices[i].ext_retail)
+        worksheet.write(f'D{6 + i}', indices[i].industry)
+        worksheet.write(f'E{6 + i}', indices[i].services)
+
+    return workbook, worksheet
 
 # Returns period string going from chosen start date to current month
 def get_period():
@@ -41,7 +76,7 @@ def get_data(period : str):
         header="n",
         format="list",
         period=period,
-        classifications={"11046": "56735"}
+        classifications={"11046": "56736"}
     )
 
     industry = get_table(
@@ -63,7 +98,7 @@ def get_data(period : str):
         header="n",
         format="list",
         period=period,
-        classifications={"11046": "56725"}
+        classifications={"11046": "56726"}
     )
 
     retail = api_to_list(retail) 
@@ -80,13 +115,14 @@ def get_data(period : str):
     
     return indices
 
+
 # Gets api data and returns it as a list of dicts
 def api_to_list(list: list):
     
     out_list = []
 
     for i in range(len(list)):
-        out_list.append({"date": list[i]["D2C"], "value": list[i]["V"]})
+        out_list.append({"date": list[i]["D2C"], "value": float(list[i]["V"])})
 
     return out_list
 
